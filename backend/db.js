786287219edcdd021f0b313598e1c036e5dbfcee
varunrajput27 <varunaarya709@gmail.com>
2025-07-30 17:55:56 +0530

    four

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // MongoDB Local
-const mongoURL = process.env.MONGODB_LOCAL_URL;
+//const mongoURL = process.env.MONGODB_LOCAL_URL;
 
 // MongoDB Atlas 
-//const mongoURL = process.env.MONGODB_URL;
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL)
   .then(() => console.log("MongoDB connected successfully"))
